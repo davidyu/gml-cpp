7de3f71c28516f0b71b6198e24115873ad2ae951
David Yu <lewen.yu@gmail.com>
2015-12-09 19:39:01 -0800

    operator tests

--- test/vector.cpp
+++ test/vector.cpp
@@ -51,3 +51,61 @@ TEST_CASE( "structured vector accessors", "" ) {
     REQUIRE( xyzw.xyz.y == 10 );
     REQUIRE( xyzw.xyz.z == 11 );
 }
+
+TEST_CASE( "vector operations", "" ) {
+    Vec2 a = { 1, 2 };
+    Vec2 b = { 3, 4 };
+    Vec2 c = a + b;
+
+    REQUIRE( c.x == 4 );
+    REQUIRE( c.y == 6 );
+
+    Vec3 d = { 5, 6, 7 };
+    Vec3 e = { 8, 9, 10 };
+    Vec3 f = d * e;
+
+    REQUIRE( f.x == 40 );
+    REQUIRE( f.y == 54 );
+    REQUIRE( f.z == 70 );
+
+    Vec4 g = { 15, 16, 17, 18 };
+    Vec4 h = { 11, 12, 13, 14 };
+    Vec4 i = g - h;
+
+    REQUIRE( i.x == 4 );
+    REQUIRE( i.y == 4 );
+    REQUIRE( i.z == 4 );
+    REQUIRE( i.w == 4 );
+
+    Vec<5> j = { 19 * 2, 20 * 2, 21 * 2, 22 * 2, 23 * 2 };
+    Vec<5> k = { 19, 20, 21, 22, 23 };
+    Vec<5> l = j / k;
+
+    REQUIRE( l[0] == 2 );
+    REQUIRE( l[1] == 2 );
+    REQUIRE( l[2] == 2 );
+    REQUIRE( l[3] == 2 );
+    REQUIRE( l[4] == 2 );
+
+    Vec<6> m = { 19 * 2, 20 * 2, 21 * 2, 22 * 2, 23 * 2, 24 * 2 };
+    float n = 2;
+    Vec<6> o = m / n;
+
+    REQUIRE( o[0] == 19 );
+    REQUIRE( o[1] == 20 );
+    REQUIRE( o[2] == 21 );
+    REQUIRE( o[3] == 22 );
+    REQUIRE( o[4] == 23 );
+    REQUIRE( o[5] == 24 );
+
+    Vec<6> p = { 19, 20, 21, 22, 23, 24 };
+    float q = 2;
+    Vec<6> r = p * q;
+
+    REQUIRE( r[0] == 38 );
+    REQUIRE( r[1] == 40 );
+    REQUIRE( r[2] == 42 );
+    REQUIRE( r[3] == 44 );
+    REQUIRE( r[4] == 46 );
+    REQUIRE( r[5] == 48 );
+}
