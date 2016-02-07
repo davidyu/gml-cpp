7811bd8c31873517733c5883a36b66bfb7cde2ac
David Yu <lewen.yu@gmail.com>
2016-02-06 21:52:38 -0800

    constructors for Ray

--- src/primitives.hpp
+++ src/primitives.hpp
@@ -5,6 +5,17 @@
 struct Ray {
     Vec3 origin;
     Vec3 direction;
+
+    Ray()
+        : origin( Vec3( 0, 0, 0 ) )
+        , direction( Vec3( 0, 0, -1 ) )
+    {}
+
+    Ray( Vec3 o, Vec3 d )
+        : origin( o )
+        , direction( d )
+    {}
+
     inline Vec3 at( float t ) const { return origin + t * direction; }
 };
 
