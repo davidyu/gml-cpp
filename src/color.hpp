c7e8e1abfc291932139c6d74aa516cda24d6f1f1
David Yu <lewen.yu@gmail.com>
2016-04-15 18:45:17 -0700

    add degamma method for colors

--- src/color.hpp
+++ src/color.hpp
@@ -7,3 +7,12 @@ typedef Vec<4> Color;
 const Color white             = Vec4( 1, 1, 1, 1 );
 const Color black             = Vec4( 0, 0, 0, 1 );
 const Color transparent_black = Vec4( 0, 0, 0, 0 );
+
+Color degamma( Color in );
+
+template <int n>
+Vec<n> degamma( Vec<n> in ) {
+    Vec<n> out = in;
+    out ^= ( 1.0 / 2.2 );
+    return out;
+}
