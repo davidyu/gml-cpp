03b8416544fbdcd608572dcbdbbc51885b0b23c9
David Yu <lewen.yu@gmail.com>
2016-04-15 19:19:48 -0700

    only degamma for Vec3 and Vec4s

--- src/color.hpp
+++ src/color.hpp
@@ -8,10 +8,11 @@ const Color white             = Vec4( 1, 1, 1, 1 );
 const Color black             = Vec4( 0, 0, 0, 1 );
 const Color transparent_black = Vec4( 0, 0, 0, 0 );
 
-Color degamma( Color in );
+template <int n> Vec<n> degamma( Vec<n> in );
 
 template <int n>
 Vec<n> degamma( Vec<n> in ) {
+    static_assert( n == 3 || n == 4, "degamma only defined for Vec<3>s and Vec<4>s!" );
     Vec<n> out = in;
     out ^= ( 1.0 / 2.2 );
     return out;
