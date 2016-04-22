141f8894ffa57eb87f1209cd507303da4430399c
David Yu <lewen.yu@gmail.com>
2016-04-21 19:38:25 -0700

    nicer interface for degamma

--- src/color.hpp
+++ src/color.hpp
@@ -8,12 +8,11 @@ const Color white             = Vec4( 1, 1, 1, 1 );
 const Color black             = Vec4( 0, 0, 0, 1 );
 const Color transparent_black = Vec4( 0, 0, 0, 0 );
 
-template <int n> Vec<n> degamma( Vec<n> in );
+Color degamma( Color in );
 
-template <int n>
-Vec<n> degamma( Vec<n> in ) {
-    static_assert( n == 3 || n == 4, "degamma only defined for Vec<3>s and Vec<4>s!" );
-    Vec<n> out = in;
+inline Color degamma( Color in ) {
+    Vec4 out = in;
     out ^= ( 1.0 / 2.2 );
+    out.a = in.a;
     return out;
 }
