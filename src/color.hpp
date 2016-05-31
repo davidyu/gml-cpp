e4edbd5b26dcabfd1a40f11e7339b783345a1d7b
David Yu <lewen.yu@gmail.com>
2016-05-31 14:57:40 -0700

    add engamma

--- src/color.hpp
+++ src/color.hpp
@@ -9,8 +9,16 @@ const Color black             = Vec4( 0, 0, 0, 1 );
 const Color transparent_black = Vec4( 0, 0, 0, 0 );
 
 Color degamma( Color in );
+Color engamma( Color in );
 
-inline Color degamma( Color in ) {
+inline Color engamma( const Color in ) {
+    Vec4 out = in;
+    out ^= 2.2;
+    out.a = in.a;
+    return out;
+}
+
+inline Color degamma( const Color in ) {
     Vec4 out = in;
     out ^= ( 1.0 / 2.2 );
     out.a = in.a;
